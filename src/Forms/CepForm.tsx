2dcee81e29323468d7c9d9aea047326bca0d0c12
Luiz Fernando <Luiz Fernando>
2025-07-24 02:17:11 -0300

    CepForm

--- src/Forms/CepForm.tsx
+++ src/Forms/CepForm.tsx
@@ -4,5 +4,44 @@ import { toast } from "react-toastify";
 import type { CepResponse } from "../Models/CepResponse";
 
 export function CepForm(){
-    
-}
+
+    const [cep, setCep] = useState("");
+    const [logradouro, setLogradouro] = useState<CepResponse>();
+    const cepService = new CepService();
+
+    const pesquisaCep = async () =>{
+
+        try {
+            const resp = await cepService.BuscarCep(cep);
+            setLogradouro(resp);
+            toast.success(`Informações para o CEP ${cep} encontradas! `, { position: 'bottom-center',});
+        } catch (error: any) {
+            toast.error(` Erro ao buscar informações para o CEP ${cep} ` +  error.message, { position: 'bottom-center',});
+        }
+  
+    }
+            return (
+            <div style={{ maxWidth: 400 }}>
+                <input
+                    type="text"
+                    placeholder="Digite o CEP"
+                    value={cep}
+                    onChange={(e) => setCep(e.target.value)}
+                    style={{ width: "100%", marginBottom: 8 }}
+            />
+            
+            <button onClick={pesquisaCep}>Buscar</button>
+
+                {logradouro?.logradouro && (
+                    <div style={{ marginTop: 16 }}>
+                        <p><strong>Rua:</strong> {logradouro!.logradouro}</p>
+                        <p><strong>Bairro:</strong> {logradouro!.bairro}</p>
+                        <p><strong>Cidade:</strong> {logradouro!.localidade} - {logradouro!.uf}</p>
+                        <p><strong>Estado:</strong> {logradouro!.uf}</p>
+                        <p><strong>Região:</strong> {logradouro!.regiao}</p>
+                        <p><strong>Código de área:</strong> {logradouro!.ddd}</p>
+                    </div>
+                )}
+            </div>
+         );
+    }   
--- src/Models/CepResponse.tsx
+++ src/Models/CepResponse.tsx
@@ -1,13 +1,16 @@
 export interface CepResponse {
-  cep: string;
-  logradouro: string;
-  complemento: string;
-  bairro: string;
-  localidade: string;
-  uf: string;
-  ibge: string;
-  gia: string;
-  ddd: string;
-  siafi: string;
+      cep: string,
+      logradouro: string,
+      complemento: string,
+      unidade: string,
+      bairro: string,
+      localidade: string,
+      uf: string,
+      estado: string,
+      regiao: string,
+      ibge: string,
+      gia: string,
+      ddd: string,
+      siafi: string,
   erro?: boolean;
 }
